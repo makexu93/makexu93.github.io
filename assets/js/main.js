c4f1da662e2296205fbd3b2b5f596cd928cea710
Marco Xu <marcoxu@gmx.de>
2022-11-13 21:15:23 +0100

    Add files via upload

--- assets/js/main.js
+++ assets/js/main.js
@@ -171,3 +171,56 @@ document.addEventListener('DOMContentLoaded', () => {
   });
 
 });
+
+
+
+
+
+/* https://stackoverflow.com/questions/26468898/change-bootstrap-column-order-randomly */
+/**
+ * Returns a random integer between min and max values (inclusive)
+ */
+function getRandomInt(min, max) {
+  return Math.floor(Math.random() * (max - min + 1)) + min;
+}
+
+/**
+ * Returns a uniquely generated random integer array, so we can use this as our order.
+ */
+function getRandomSequence(limit) {
+  var arr = [];
+  while (arr.length < limit) {
+    var randomnumber = getRandomInt(0, limit - 1);
+    var found = false;
+    for (var i = 0; i < arr.length; i++) {
+      if (arr[i] == randomnumber) {
+        found = true;
+        break;
+      }
+    }
+    if (!found) arr[arr.length] = randomnumber;
+  }
+  return arr;
+}
+
+/**
+ * Shows randomly ordered columns in a container.
+ */
+function orderColumnsRandomly() {
+  // Get the children of the container
+  var columns = $("#xcontainer").children();
+
+  // Empty the container.
+  $("#xcontainer").html('');
+
+  // Get a random sequence to show the columns
+  var sequence = getRandomSequence(columns.length);
+
+  // Loop through the column array with the given sequence and fill the container.
+  for (var i = 0; i < sequence.length; i++) {
+    $("#xcontainer").append($(columns[sequence[i]]));
+  }
+}
+
+// execute everytime when page refreshes
+orderColumnsRandomly();
